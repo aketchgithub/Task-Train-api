c40520febaffc664ad20a1dfa7318b5bb3e34411
Victor Aketch <vickeryace@gmail.com>
2023-03-28 09:32:05 +0300

    Complete api build

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# require 'faker'
+
+# # Create 10 users
+# puts "📃 Seeding data..."
+# 10.times do
+#   User.create!(
+#     username: Faker::Alphanumeric.alpha(number: 8),
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password(min_length: 8),
+#   )
+# end
+
+# # Create 20 todos for each user
+# User.all.each do |user|
+#   10.times do
+#     Todo.create!(
+#         title: Faker::Lorem.words(number: 2).join(' '),
+#         description: Faker::Lorem.sentence(word_count: 20),
+#         status: ['CREATED', 'STARTED', 'COMPLETED', 'CANCELLED'].sample,
+#         priority: ['LOW', 'MEDIUM', 'HIGH' ].sample,
+#         user: User.order(Arel.sql('RANDOM()')).first
+#     )
+#   end
+# end
+
+# puts "✅ Done seeding"
