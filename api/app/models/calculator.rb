b485cf883e02b124ea15f59bc444ecfce381ec72
Victor Aketch <vickeryace@gmail.com>
2023-03-28 10:36:36 +0300

    Changes in tasktrain

--- api/app/models/calculator.rb
+++ api/app/models/calculator.rb
@@ -1,32 +1,34 @@
-# class Calculator
-#     attr_accessor :n1, :n2
-#     def initialize(n1: ,n2:)
-#         @n1 = n1
-#         @n2 = n2
-#     end
+#test
 
-#     def add
-#         @n1 + @n2
-#     end
+class Calculator
+    attr_accessor :n1, :n2
+    def initialize(n1: ,n2:)
+        @n1 = n1
+        @n2 = n2
+    end
 
-#     def sub
-#         @n1 - n2
-#     end
+    def add
+        @n1 + @n2
+    end
 
-#     def divide
-#         @n1 / @n2
-#     end
+    def sub
+        @n1 - n2
+    end
 
-#     def multiply
-#         @n1 * @n2
-#     end
+    def divide
+        @n1 / @n2
+    end
 
-#     def square
-#         @n1 * @n1
-#     end
+    def multiply
+        @n1 * @n2
+    end
 
-#     def cube
-#         @n1 * @n1 * @n1
-#     end
+    def square
+        @n1 * @n1
+    end
 
-# end
+    def cube
+        @n1 * @n1 * @n1
+    end
+
+end
