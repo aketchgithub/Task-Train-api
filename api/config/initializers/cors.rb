ec3b627449c2a4d36e550e5c1bf8833b5cd7ff5c
Victor Aketch <vickeryace@gmail.com>
2023-03-28 14:20:59 +0300

    Changes Rack cors configuration

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "https://task-train-api.onrender.com", "https://task-train-api.onrender.com"
 
     resource "*",
       headers: :any,
